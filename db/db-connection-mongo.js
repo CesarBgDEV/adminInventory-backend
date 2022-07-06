3a18fd1946f066d1926f6adac2de9fec61d79c03
CesarBgDEV <cesar.softwared@gmail.com>
2022-07-06 16:56:02 -0500

    Commit

---
+++ db/db-connection-mongo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+ const getConnection = async ()=>{
+    try{
+        const url = 'mongodb://user_bd:<password>@ac-4d9wfui-shard-00-00.yy0cop7.mongodb.net:27017,ac-4d9wfui-shard-00-01.yy0cop7.mongodb.net:27017,ac-4d9wfui-shard-00-02.yy0cop7.mongodb.net:27017/inventario?ssl=true&replicaSet=atlas-srii52-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+        await mongoose.connect(url);
+    
+        console.log('Conexión exitosa...');
+
+    }catch(error){
+        console.log(error);
+    }
+
+   
+ }
+
+ module.exports ={
+    gerConnection,
+ }
