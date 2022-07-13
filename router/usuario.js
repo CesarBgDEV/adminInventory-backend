38dc38fecf00aec8923d735ea811f498f67a4bcd
CesarBgDEV <cesar.softwared@gmail.com>
2022-07-13 16:56:24 -0500

    Se cambió el puerto por 4000

--- router/usuario.js
+++ router/usuario.js
@@ -2,45 +2,46 @@ const { Router } = require('express');
 const router = Router();
 const Usuario = require('../models/Usuario');
 const {validarUsuario} = require('../helpers/validar-usuario');
-
+const {validationResult, check} = require('express-validator');
 
 
 
 //Crear usuario
-router.post('/', async function(req,res){
-    try{
+router.post('/',
+    async function(req,res){
+        try{
 
-        const validaciones = validarUsuario(req);
-        if(validaciones.length > 0){
-            return res.status(400).send(validaciones);
-        }
+            const validaciones = validarUsuario(req);
+            if(validaciones.length > 0){
+                return res.status(400).send(validaciones);
+            }
+            
+            console.log(req.body);
+
+            //Validar número empleado
+            const existeUsuario = await Usuario.findOne({ numero: req.body.numero });
+            console.log('RESPUESTA EXISTE USUARIO: ',existeUsuario);
+
+            if(existeUsuario){
+                return res.status(400).send("Numero de empleado ya existe");
+            }
+
+            let usuario = new Usuario();
+            usuario.nombre = req.body.nombre;
+            usuario.numero = req.body.numero;
+            usuario.puesto = req.body.puesto;
+            usuario.estado = req.body.estado;
+            usuario.fechaCreacion = new Date();
+            usuario.fechaActualizacion = new Date();
         
-        console.log(req.body);
+            usuario =  await usuario.save();
+        
+            res.send(usuario);
 
-        //Validar número empleado
-        const existeUsuario = await Usuario.findOne({ numero: req.body.numero });
-        console.log('RESPUESTA EXISTE USUARIO: ',existeUsuario);
-
-        if(existeUsuario){
-            return res.status(400).send("Numero de empleado ya existe");
+        }catch(error){
+            console.log(error);
+            res.status(500).send('Ocurrio un error al consultar usuario')
         }
-
-        let usuario = new Usuario();
-        usuario.nombre = req.body.nombre;
-        usuario.numero = req.body.numero;
-        usuario.puesto = req.body.puesto;
-        usuario.estado = req.body.estado;
-        usuario.fechaCreacion = new Date();
-        usuario.fechaActualizacion = new Date();
-    
-        usuario =  await usuario.save();
-    
-        res.send(usuario);
-
-    }catch(error){
-        console.log(error);
-        res.status(500).send('Ocurrio un error al consultar usuario')
-    }
 
 
 });
