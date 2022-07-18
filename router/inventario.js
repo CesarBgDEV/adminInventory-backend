8e29aae5676302134d9fb388f5238b1654767540
CesarBgDEV <cesar.softwared@gmail.com>
2022-07-18 17:06:14 -0500

    CREACIÓN DE LA RUTA GET PARA INVENTARIO ID

--- router/inventario.js
+++ router/inventario.js
@@ -108,4 +108,18 @@ router.put('/:inventarioId', async function(req,res){
     }
 });
 
+
+router.get('/:inventarioId', async function(req, res){
+    try {
+        const inventario = await Inventario.findById(req.params.inventarioId);
+        if(!inventario){
+            return res.status(404).send('Inventario no existe');
+        }
+        res.send(inventario);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ocurrio un erro al consultar inventarios')
+    }
+})
+
 module.exports = router;
